a5610b67064e5828bc51dc45aeb6173004df2000
Adam Zajíček <adam.zajicek.020@pslib.cz>
2022-10-14 09:31:38 +0200

    broken

--- game.js
+++ game.js
@@ -17,50 +17,76 @@ let blackCells = [];
 let threatactoroneCells = [];
 let threatactortwoCells = [];
 
+let mogal = document.getElementById("mogal");
+let mogalText = document.getElementById("mogal-text");
+
+let correctButton = document.getElementById("correct");
+let wrongButton = document.getElementById("wrong");
+
 let turn = false; //false - threat actor one, true - threat actor two
 
-function InitField(){
+function InitField() {
     let i = 0;
-    array.forEach(element => {
-        element.addEventListener('click', e => {
+    susmoguscells.forEach(element => {
+        element.addEventListener('click', function InitCell() {
             TurnManager();
-            CellManager(i);
+            CellManager(susmoguscells.indexOf(element));
         })
+        element.innerHTML = (susmoguscells.indexOf(element) + 1)
         i++;
     });
+    correctButton.addEventListener('click', e => {
+        let _cell = document.getElementsByClassName("selected")[0];
+        if (_cell) //if the document has an element by class of selected
+        {
+            if (!turn) {
+                threatactoroneCells.push(_cell);
+                susmoguscells.splice(susmoguscells.indexOf(_cell), 1);
+            }
+            else {
+            }
+        }
+        _cell.removeEventListener('click', e);
+    })
+    wrongButton.addEventListener('click', e => {
+        let _cell = document.getElementsByClassName("selected")[0];
+        if (_cell) {
+        }
+    })
 }
 
-function TurnManager()
-{
-    if(!turn)
-    {
+function TurnManager() {
+    if (!turn) {
         ChangeColor(firstLabel, secondLabel);
         turn = true;
     }
-    else{
+    else {
         ChangeColor(secondLabel, firstLabel);
         turn = false;
     }
 }
-function ChangeColor(element, element2)
-{
+function ChangeColor(element, element2) {
     element.style.color = "rgb(234, 74, 111)";
     element.style.transform = "scale(1.5)";
 
     element2.style.color = "white";
     element2.style.transform = "scale(1)";
 }
-function CellManager(cell_index){
+function CellManager(cell_index) {
     let cell = susmoguscells[cell_index];
+    cell.classList.add("selected");
     if (!turn) { //Threatactorone
-        if(!susmoguscells.includes(cell)){ //Threatactorone kliknul na prázdné
-            
+        if (susmoguscells.includes(cell)) { //Threatactorone kliknul na prázdné
+            mogal.style.opacity = 1;
+            let rnd = Math.floor(Math.random() * otazkyDefault.length);
+            mogalText.innerText = otazkyDefault[rnd];
         }
-        else{ // černý pole
-            
+        else { // černý pole
+
         }
-    } else {
-        
+    }
+    else { //Threatactortwo
+
     }
 
     //if cell - nemá přidělení - zobrazí otázku - ano/ne x default
@@ -69,6 +95,5 @@ function CellManager(cell_index){
     //ano/ne - přidělí se jednomu z hráčů
 
 }
-
 TurnManager();
 InitField();
